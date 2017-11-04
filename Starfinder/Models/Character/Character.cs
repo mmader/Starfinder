73b653b3f861aaf1a212f854341843924bf64bde
mmader <skull17@gmx.ch>
2017-11-04 13:57:25 +0100

    Upload Works

--- Starfinder/Models/Character/Character.cs
+++ Starfinder/Models/Character/Character.cs
@@ -10,18 +10,19 @@ namespace Starfinder.Models
 		#region Properties
 		public int Id { get; set; }
 
+		public string Name  { get; set; } = "New Character";
+		public int    Level { get; set; } = 1;
 
-		public string Name { get; set; } = "New Character";
-		public int Level { get; set; } = 1;
-
-		public int Strength { get; set; } = 10;
-		public int Dexterity { get; set; } = 10;
+		public int Strength     { get; set; } = 10;
+		public int Dexterity    { get; set; } = 10;
 		public int Constitution { get; set; } = 10;
-		public int Wisdom { get; set; } = 10;
+		public int Wisdom       { get; set; } = 10;
 		public int Intelligence { get; set; } = 10;
 
-		public Race Race { get; set; }
+		public Race           Race  { get; set; }
 		public CharacterClass Class	{ get; set; }
+
+		public string AvatarPath { get; set; }
 		#endregion
 
 
@@ -32,6 +33,11 @@ namespace Starfinder.Models
 		}
 
 		internal static Character Create() => new Character();
+		#endregion
+
+
+		#region Overrides
+		public override string ToString() => $"Name: {Name} - {Level}";
 		#endregion
 	}
 }
